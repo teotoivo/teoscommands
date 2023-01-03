f30c3c2986e4a2027502d73ebb224718d2ad7e6e
teotoivo <teo.maximilien@gmail.com>
2023-01-03 13:56:26 +0200

    added experimental/unfinished features and fixed some bugs

--- src/buildReactProject.ts
+++ src/buildReactProject.ts
@@ -1,220 +1,225 @@
-import { execSync } from 'child_process';
-import * as fs from 'fs';
-import path = require('path');
-import * as vscode from 'vscode';
+import { execSync } from "child_process";
+import * as fs from "fs";
+import path = require("path");
+import * as vscode from "vscode";
 
 export async function buildReactProject(context: vscode.ExtensionContext) {
+  return vscode.commands.registerCommand(
+    "teoscommands.buildReactProject",
+    async () => {
+      async function getProjectName(): Promise<string | undefined> {
+        return await vscode.window.showInputBox({
+          placeHolder: "enter project name",
+        });
+      }
 
-	
+      let dir: string | undefined =
+        (() => {
+          if (vscode.workspace.workspaceFolders) {
+            return vscode.workspace.workspaceFolders[0].uri.fsPath;
+          }
+          return undefined;
+        })() ?? "no dir open";
 
-	return vscode.commands.registerCommand('teoscommands.buildReactProject', async () => {
+      // Parse the project name from the command line arguments
+      //const projectName = process.argv[2];
 
+      let projectName = await getProjectName();
+      if (projectName === undefined) {
+        return;
+      }
 
-		async function getProjectName(): Promise<string | undefined> {
-			return await vscode.window.showInputBox({
-				placeHolder: 'enter project name',
-			});
-		}
+      //check if there is a project with the same name
+      if (fs.existsSync(path.join(dir, projectName))) {
+        vscode.window.showErrorMessage(`Project ${projectName} already exists`);
+        return;
+      }
 
-		let dir: string | undefined = (() => {
-			if (vscode.workspace.workspaceFolders) {
-				return vscode.workspace.workspaceFolders[0].uri.fsPath;
-			}
-			return undefined;
-		})() ?? "no dir open";
-	
-		// Parse the project name from the command line arguments
-		//const projectName = process.argv[2];
-		
-		let projectName = await getProjectName();
-		if (projectName === undefined) {
-			return;
-		}
+      let projectdir = path.join(dir, projectName);
 
-		//check if there is a project with the same name
-		if (fs.existsSync(path.join(dir, projectName))) {
-			vscode.window.showErrorMessage(`Project ${projectName} already exists`);
-			return;
-		}
+      // open new terminal
+      const term: vscode.Terminal = vscode.window.createTerminal(projectName);
+      term.show();
 
-		let projectdir = path.join(dir, projectName);
+      // Create a new directory for the project
+      fs.mkdirSync(projectdir);
 
-		// open new terminal
-		const term: vscode.Terminal = vscode.window.createTerminal(projectName);
-		term.show();
+      //change terminal directory
+      term.sendText(`cd ${projectdir}`);
 
-		// Create a new directory for the project
-		fs.mkdirSync(projectdir);
+      // Navigate into the new project directory
 
-		//change terminal directory
-		term.sendText(`cd ${projectdir}`);
+      process.chdir(path.join(dir, projectName));
 
-		// Navigate into the new project directory
-		
-		process.chdir(path.join(dir, projectName));
+      // Initialize a new npm package
+      fs.writeFileSync(
+        "package.json",
+        JSON.stringify(
+          {
+            name: projectName,
+            version: "1.0.0",
+            private: true,
+            scripts: {
+              build: "webpack --config webpack.config.js",
+              start: "webpack-dev-server --config webpack.config.js",
+            },
+          },
+          null,
+          4
+        ),
+        "utf-8"
+      );
 
-	
-	
-		// Initialize a new npm package
-		fs.writeFileSync(
-			'package.json',
-			JSON.stringify({
-			  	name: projectName,
-			  	version: '1.0.0',
-			  	private: true,
-			  	scripts: {
-					build: 'webpack --config webpack.config.js',
-					start: 'webpack-dev-server --config webpack.config.js',
-			  	},
-			}, null, 4),
-			'utf-8'
-		);
-		
-		// Install React and React DOM as dependencies
-		execSync(`cd "${projectdir}"&npm install react react-dom`, { stdio: 'inherit' });
-		
-		// Install TypeScript and the TypeScript React Preset as dev dependencies
-		execSync(`cd "${projectdir}"&npm install --save-dev typescript @types/react @types/react-dom @types/node`, { stdio: 'inherit' });
+      // Install React and React DOM as dependencies
+      execSync(`cd "${projectdir}"&npm install react react-dom`, {
+        stdio: "inherit",
+      });
 
-		//babel
-		execSync(`cd "${projectdir}"&npm install --save-dev @babel/core @babel/preset-env @babel/preset-react babel-loader`, { stdio: 'inherit' });
+      // Install TypeScript and the TypeScript React Preset as dev dependencies
+      execSync(
+        `cd "${projectdir}"&npm install --save-dev typescript @types/react @types/react-dom @types/node`,
+        { stdio: "inherit" }
+      );
 
-		// Install TypeScript, Babel, and the required Babel presets and plugins as dev dependencies
-		execSync(`cd "${projectdir}"&npm install --save-dev typescript @babel/preset-react @babel/preset-typescript @babel/plugin-proposal-class-properties`, {
-			stdio: 'inherit',
-		});
+      //babel
+      execSync(
+        `cd "${projectdir}"&npm install --save-dev @babel/core @babel/preset-env @babel/preset-react babel-loader`,
+        { stdio: "inherit" }
+      );
 
-		// install webpack and webpack-cli as dev dependencies
-		execSync(`cd "${projectdir}"&npm install --save-dev webpack webpack-cli webpack-dev-server ts-loader html-webpack-plugin`, { stdio: 'inherit' });
+      // Install TypeScript, Babel, and the required Babel presets and plugins as dev dependencies
+      execSync(
+        `cd "${projectdir}"&npm install --save-dev typescript @babel/preset-react @babel/preset-typescript @babel/plugin-proposal-class-properties`,
+        {
+          stdio: "inherit",
+        }
+      );
 
-		// Create a basic TypeScript configuration file
-		fs.writeFileSync(
-		  'tsconfig.json',
-		  JSON.stringify({
-			compilerOptions: {
-			  jsx: 'react',
-			  lib: ['dom', 'esnext'],
-			  module: 'esnext',
-			  moduleResolution: 'node',
-			  noImplicitAny: true,
-			  outDir: 'build',
-			  sourceMap: true,
-			  strict: true,
-			  target: 'es5',
-			},
-			include: ['src'],
-			exclude: ['node_modules'],
-		  }, null, 4),
-		  'utf-8'
-		);
+      // install webpack and webpack-cli as dev dependencies
+      execSync(
+        `cd "${projectdir}"&npm install --save-dev webpack webpack-cli webpack-dev-server ts-loader html-webpack-plugin`,
+        { stdio: "inherit" }
+      );
 
-		//create .bavlerc file
-		fs.writeFileSync(
-			'.babelrc',
-			JSON.stringify({
-				presets: ['@babel/preset-react', '@babel/preset-typescript'],
-				plugins: ['@babel/plugin-proposal-class-properties'],
-			}, null, 4),
-			'utf-8'
-		);
+      // Create a basic TypeScript configuration file
+      fs.writeFileSync(
+        "tsconfig.json",
+        JSON.stringify(
+          {
+            compilerOptions: {
+              allowSyntheticDefaultImports: true,
+              jsx: "react",
+              lib: ["dom", "esnext"],
+              module: "esnext",
+              moduleResolution: "node",
+              noImplicitAny: true,
+              outDir: "build",
+              sourceMap: true,
+              strict: true,
+              target: "es5",
+            },
+            include: ["src"],
+            exclude: ["node_modules"],
+          },
+          null,
+          4
+        ),
+        "utf-8"
+      );
 
+      //create .bavlerc file
+      fs.writeFileSync(
+        ".babelrc",
+        JSON.stringify(
+          {
+            presets: ["@babel/preset-react", "@babel/preset-typescript"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+          null,
+          4
+        ),
+        "utf-8"
+      );
 
+      // Create a basic Webpack configuration file
+      fs.writeFileSync(
+        "webpack.config.js",
+        `
 
-		// Create a basic Webpack configuration file
-		fs.writeFileSync(
-			'webpack.config.js',
-			`
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-	mode: 'development',
-	entry: './src/index.tsx',
-	devtool: 'inline-source-map',
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'bundle.js'
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './dist',
-		open: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /.jsx?$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	plugins:[
-		new HtmlWebpackPlugin({
-		template: './public/index.html'
-		})
-	]
-}
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+	path: path.join(__dirname, "/dist"),
+	filename: "bundle.js",
+
+	//new
+	publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+	static: "./dist",
+	open: true,
+	//new
+	historyApiFallback: true,
+  },
+  module: {
+	rules: [
+	  {
+		test: /.jsx?$/,
+		exclude: /node_modules/,
+		loader: "babel-loader",
+	  },
+	  {
+		test: /.tsx?$/,
+		use: "ts-loader",
+		exclude: /node_modules/,
+	  },
+	],
+  },
+  resolve: {
+	extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+	new HtmlWebpackPlugin({
+	  template: "./public/index.html",
+	}),
+  ],
+};
+		
 
 		  `,
-			'utf-8'
-		);
-	
-		// Create the basic file structure for the project
-		fs.mkdirSync('src');
-		fs.mkdirSync('public');
-	
-		// Create a basic HTML file
-		fs.writeFileSync(
-		  'public/index.html',
-		  `
-<!DOCTYPE html>
-<html>
-  <head>
-	<meta charset='utf-8'>
-	<title>${projectName}</title>
-  </head>
-  <body>
-	<div id='root'></div>
-  </body>
-</html>
-		`,
-		  'utf-8'
-		);
-		
-		// Create a basic TypeScript entry point file
-		fs.writeFileSync(
-		  'src/index.tsx',
-		  `
+        "utf-8"
+      );
+
+      // Create the basic file structure for the project
+      fs.mkdirSync("src");
+      fs.mkdirSync("public");
+
+      // Create a basic TypeScript entry point file
+      fs.writeFileSync(
+        "src/index.tsx",
+        `
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 	
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-)
+const root = document.getElementById('root');
 
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+ReactDOM.render(
+	<App />,
+	root);
 
 		`,
-		  'utf-8'
-		);
+        "utf-8"
+      );
 
-		fs.writeFileSync(
-			'src/App.tsx',
-			`
+      fs.writeFileSync(
+        "src/App.tsx",
+        `
 import * as React from 'react';
 
 const App = () => {
@@ -224,10 +229,13 @@ const App = () => {
 }
 export default App;
 `,
-			'utf-8'
-		);
+        "utf-8"
+      );
 
-
-		vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(projectdir));
-	});
+      vscode.commands.executeCommand(
+        "vscode.openFolder",
+        vscode.Uri.file(projectdir)
+      );
+    }
+  );
 }
--- src/extension.ts
+++ src/extension.ts
@@ -17,30 +17,96 @@ export async function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	
-	
+	//settings
+	let config = vscode.workspace.getConfiguration('teoscommands');
 	
 
 	const teoscommandsBuildTsProject = await buildTsProject(context);
 
 	const teoscommandsBuildReactProject = await buildReactProject(context);
 
-	//create new status bar item
+	const teoscommandstrigger = vscode.commands.registerCommand('teoscommands.trigger', () => {
+		return;
+	});
+
+
 	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
 	statusBarItem.text = "Build TS Project";
 	statusBarItem.command = 'teoscommands.buildTsProject';
-	statusBarItem.show();
+
+	//create new status bar item
+	const statusBarItem3 = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    statusBarItem3.tooltip = 'run';
+    statusBarItem3.command = 'teoscommands.run';
+
+	let triangle = String.fromCharCode(9654);
+
+	statusBarItem3.text = `${triangle} Run`;
+    context.subscriptions.push(statusBarItem3);
+
+	
+	
+	if (config.get('createTsButton') as boolean) {
+		statusBarItem.show();
+	}
 
 	let statusBarItem2 = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 2);
 	statusBarItem2.text = "Build React Project";
 	statusBarItem2.command = 'teoscommands.buildReactProject';
-	statusBarItem2.show();
+
+
+	
+	if (config.get('createReactButton') as boolean) {
+		statusBarItem2.show();
+	}
+
+
+	vscode.workspace.onDidChangeConfiguration((e) => {
+		let config = vscode.workspace.getConfiguration('teoscommands');
+		
+		if (e.affectsConfiguration("teoscommands.createTsButton")) {
+			
+			if (config.get('createTsButton') as boolean) {
+
+				statusBarItem.show();
+			} else {
+
+				statusBarItem.hide();
+			}
+		}
+
+	
+		if (config.get('createReactButton') as boolean) {
+			statusBarItem2.show();
+		} else {
+			statusBarItem2.hide();
+		}
+		//trigger extension
+		vscode.commands.executeCommand('teoscommands.trigger');
+
+	});
+
+	vscode.languages.onDidChangeDiagnostics((e) => {
+		console.log("onDidChangeDiagnostics");
+		
+		const languageid = vscode.window.activeTextEditor?.document.languageId;
+		if (languageid === 'typescript') {
+			statusBarItem3.show();
+		} else if (languageid === 'javascript') {
+			statusBarItem3.show();
+		} else {
+			statusBarItem3.hide();
+		}
+		//trigger extension
+		vscode.commands.executeCommand('teoscommands.trigger');
+	});
 
 
 
 	const commands: { [key: string]: vscode.Disposable} = {
 		teoscommandsBuildTsProject,
-		teoscommandsBuildReactProject
-
+		teoscommandsBuildReactProject,
+		teoscommandstrigger
 	};
 	
 	for (const key in commands) {
